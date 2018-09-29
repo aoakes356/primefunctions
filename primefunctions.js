d49bac173152282eeb2d1b6937ffc78d6fd69b23
Andrew Oakes <aoakes356@gmail.com>
2018-09-28 22:31:38 -0700

    Finished maxPrimeSum.

--- primefunctions.js
+++ primefunctions.js
@@ -1,12 +1,12 @@
 function prime(max) {
   const primes = [2];
-  _.filter(_.range(2, max, 1),
+  _.filter(_.range(2, max, 1), // Generate a list of numbers from 2 to max.
     function (isprime) {
       if (_.every(primes,
         function (div) {
-          return (isprime % div !== 0 && isprime !== div);
+          return (isprime % div !== 0 && isprime !== div); // Checks if divisible by all currently known primes.
         })) {
-        primes.push(isprime); return true;
+        primes.push(isprime); return true;// Adds to array of primes if it passes.
       } return false;
     });
   return primes;
@@ -20,6 +20,29 @@ function cumulativeSum(numbers) {
   }
   return num;
 }
-
-
 console.log(cumulativeSum([1, 2, 3, 4]));
+
+function maxPrimeSum(max) {
+  const primes = prime(max);
+  const len = primes.length;
+  let maxSum = 0;
+  let maxCon = 0;
+  let temp;
+  let maxTemp;
+  let lenTemp;
+  let start = 0;
+  while (start < len) {
+    temp = cumulativeSum(primes.slice(start, len));
+    console.log(temp);
+    maxTemp = _.max(_.filter(temp, function (sum) { return _.indexOf(primes, sum) !== -1; }));
+    lenTemp = _.indexOf(temp, maxTemp);
+    if (lenTemp > maxCon) {
+      maxSum = maxTemp;
+      maxCon = lenTemp;
+    }
+    start++;
+  }
+  return [maxSum, maxCon];
+}
+
+console.log(maxPrimeSum(100));
