fd8572b392487650df275aa9b456349844ca39e9
Andrew Oakes <andrew.oakes@wsu.edu>
2018-10-12 16:26:47 -0700

    Figured it out finally.

--- testCorrectness.js
+++ testCorrectness.js
@@ -1,10 +1,17 @@
 /* eslint-env mocha, chai */
-/* global foo, chai */
+/* global chai, prime */
 
-describe('foo',
+
+describe('primeGen',
   function () {
-    it('must be string',
+    it('primeGen(10) = [2,3,5,7]',
       function () {
-        chai.expect(foo).to.be.a('string');
+        const primeslt10 = prime(10);
+        chai.assert.deepEqual(primeslt10, [2, 3, 5, 7]);
+      });
+    it('primeGen(20) = [2, 3, 5, 7, 11, 13, 17, 19]',
+      function () {
+        const primeslt20 = prime(20);
+        chai.assert.deepEqual(primeslt20, [2, 3, 5, 7, 11, 13, 17, 19]);
       });
   });
